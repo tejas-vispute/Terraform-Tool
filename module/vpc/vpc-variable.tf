7d10b43b27fcf96ef38344a8236b4b8c812886e8
Tejas vispute <tejasvispute1111@gmail.com>
2026-02-09 15:22:07 +0530

    Add VPC variables for CIDR blocks and availability zones

---
+++ module/vpc/vpc-variable.tf
@@ -0,0 +1,19 @@
+
+variable "vpc_cidr_block" {
+    type = string
+
+}
+
+variable "subnet_cidr_block" {
+  type = string 
+}
+
+variable "az" {
+  type = string 
+  
+}
+
+variable "public_ip" {
+  type = bool
+ 
+}
